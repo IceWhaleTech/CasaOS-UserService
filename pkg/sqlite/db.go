6c73a4be13f1590d5dd1c565309230511245627c
Tiger Wang (王豫) <tigerwang@outlook.com>
2022-08-31 17:56:39 -0400

    fix migration from v0.3.3 (#11)

--- pkg/sqlite/db.go
+++ pkg/sqlite/db.go
@@ -26,30 +26,19 @@ func GetDb(dbPath string) *gorm.DB {
 	if gdb != nil {
 		return gdb
 	}
-	// Refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
-	// dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", m.User, m.PWD, m.IP, m.Port, m.DBName)
-	// db, err := gorm.Open(mysql2.Open(dsn), &gorm.Config{})
+
 	file.IsNotExistMkDir(dbPath)
 	db, err := gorm.Open(sqlite.Open(dbPath+"/user.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
 	c, _ := db.DB()
 	c.SetMaxIdleConns(10)
 	c.SetMaxOpenConns(100)
 	c.SetConnMaxIdleTime(time.Second * 1000)
-	if err != nil {
-		logger.Error("sqlite connect error", zap.Any("db connect error", err))
-		panic("sqlite connect error")
-	}
+
 	gdb = db
-
-	db.Exec(`alter table o_user rename to old_user;
-
-	create table o_users ( id integer primary key,username text,password text,role text,email text,nickname text,avatar text,description text,created_at datetime,updated_at datetime);
-	
-	insert into o_users select id,user_name,password,role,email,nick_name,avatar,description,created_at,updated_at from old_user;
-	
-	drop table old_user;
-	drop table o_user;	
-	`)
 
 	err = db.AutoMigrate(model2.UserDBModel{})
 	if err != nil {
