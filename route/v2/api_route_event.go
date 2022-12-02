58b5f005a00af72d7191d55043b1fcb6dc4bc433
Tiger Wang <tigerwang@outlook.com>
2022-12-02 21:12:14 +0000

    wip

--- route/v2/api_route_event.go
+++ route/v2/api_route_event.go
@@ -2,13 +2,19 @@ package v2
 
 import (
 	"net/http"
+	"sync"
 
 	codegen "github.com/IceWhaleTech/CasaOS-UserService/codegen/user_service"
 	"github.com/IceWhaleTech/CasaOS-UserService/service"
 	"github.com/labstack/echo/v4"
 )
 
+var mutex sync.Mutex
+
 func (s *UserService) DeleteEvent(ctx echo.Context, params codegen.EventUuid) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	m := service.MyService.Event().GetEventByUUID(params.String())
 	service.MyService.Event().DeleteEvent(params.String())
 	return ctx.JSON(http.StatusOK, m)
@@ -18,6 +24,7 @@ func (s *UserService) GetEvents(ctx echo.Context, params codegen.GetEventsParams
 	list := service.MyService.Event().GetEvents()
 	return ctx.JSON(http.StatusOK, list)
 }
+
 func (s *UserService) DeleteEventBySerial(ctx echo.Context, serial codegen.Serial) error {
 	service.MyService.Event().DeleteEventBySerial(serial)
 	return ctx.JSON(http.StatusOK, serial)
